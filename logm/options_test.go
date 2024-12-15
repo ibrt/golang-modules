bbcbaa326fb18075bb2e1fce8944a880d94dd9bc
Ivan Bertona <ivan.bertona@gmail.com>
2024-12-15 18:26:13 +0100

    Add tests.

--- logm/options_test.go
+++ logm/options_test.go
@@ -16,17 +16,33 @@ func TestOptionsSuite(t *testing.T) {
 }
 
 func (*OptionsSuite) TestEmitOptions(g *WithT) {
-	g.Expect(newEmitOptions(EmitA(1, 2, 3), EmitM("k", "v"))).
+	g.Expect(newEmitOptions(
+		EmitA(1, 2, 3),
+		EmitM("k1", "v1"),
+		EmitMetadata{"k2": "v2"})).
 		To(Equal(&emitOptions{
-			args:     []any{1, 2, 3},
-			metadata: map[string]any{"k": "v"},
+			args: []any{1, 2, 3},
+			metadata: map[string]any{
+				"k1": "v1",
+				"k2": "v2",
+			},
 		}))
 }
 
 func (*OptionsSuite) TestBeginOptions(g *WithT) {
-	g.Expect(newBeginOptions(BeginM("k", "v"), BeginErrM("ek", "ev"))).
+	g.Expect(newBeginOptions(
+		BeginM("k1", "v1"),
+		BeginMetadata{"k2": "v2"},
+		BeginErrM("ek1", "ev1"),
+		BeginErrMetadata{"ek2": "ev2"})).
 		To(Equal(&beginOptions{
-			metadata:    map[string]any{"k": "v"},
-			errMetadata: map[string]any{"ek": "ev"},
+			metadata: map[string]any{
+				"k1": "v1",
+				"k2": "v2",
+			},
+			errMetadata: map[string]any{
+				"ek1": "ev1",
+				"ek2": "ev2",
+			},
 		}))
 }
